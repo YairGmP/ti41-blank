efe33fd0f175e5106edf7330914ee1f7f60984ac
YairGmP <2302133@utrivieramaya.edu.mx>
2025-03-07 01:51:43 -0500

    Conexion de backend con frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { RouteReuseStrategy } from '@angular/router';
 import {TaxPipe} from './pipes/tax.pipe';
 import {HomePage} from './home/home.page';
@@ -7,6 +8,7 @@ import { UserComponent } from './component/user/user.component';
 import {HighlightDirective} from './highlight.directive';
 import {ReactiveFormsModule} from '@angular/forms';
 import {LocalStorageService} from './services/local-storage.service';
+import { HttpService } from './services/http.service';
 
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
@@ -17,10 +19,11 @@ import {CalcComponent} from './component/calc/calc.component'
 import { LoginComponent } from './component/login/login.component';
 
 
+
 @NgModule({
   declarations: [AppComponent, HomePage, UserComponent, CalcComponent, LoginComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, TaxPipe, HighlightDirective, ReactiveFormsModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, LocalStorageService],
+  imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule, TaxPipe, HighlightDirective, ReactiveFormsModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, LocalStorageService, HttpService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
-import { FormControl } from '@angular/forms';
 import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
+import { HttpService } from 'src/app/services/http.service';
+
 
 
 @Component({
@@ -10,32 +12,50 @@ import { LocalStorageService } from 'src/app/services/local-storage.service';
   styleUrls: ['./login.component.scss'],
   standalone: false,
 })
-export class LoginComponent  implements OnInit {
+export class LoginComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private localStorageServices: LocalStorageService
-  ) {
-
-   }
+    private localStorageServices: LocalStorageService,
+    private httpService: HttpService
+  ) { }
 
   public user = new FormControl('');
-
   public password = new FormControl('');
 
   ngOnInit() {
     this.localStorageServices.setItem('logedIn', false);
   }
 
-  login(){
-    console.log('User: ', this.user.value);
-    console.log('Password: ', this.password.value);
-    if(this.user.value === 'admin' && this.password.value === 'Admin123'){
-      this.localStorageServices.setItem('logedIn', true);
-      this.router.navigate(['/home']);
-    }else{
-      alert('User or Password incorrect');
+  login() {
+
+    const payload = {
+      condition: {
+        name: this.user.value,
+        password: this.password.value,
+      }
     }
+
+    this.httpService.post('get-user', payload).subscribe((response: any) => {
+      console.log('Response from server', response);
+
+      if (response && response.data && response.data.length > 0) {
+        this.localStorageServices.setItem('logedIn', true);
+        this.router.navigate(['/home']);
+      } else {
+        alert(response.message);
+      }
+    })
+
+
+    // console.log('User: ', this.user.value);
+    // console.log('Password: ', this.password.value);
+    // if(this.user.value === 'tortis' && this.password.value === '21210901'){
+    //   this.localStorageServices.setItem('logedIn', true);
+    //   this.router.navigate(['/home']);
+    // }else{
+    //   alert('User or Password incorrect');
+    // }
     // return;
   }
 
